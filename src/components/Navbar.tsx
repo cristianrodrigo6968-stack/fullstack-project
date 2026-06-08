a49487cfb4aaddb5c453fc4eecd0b4ed098e32dd
Cristian <tu_correo@gmail.com>
2026-06-08 03:02:06 -0400

    Integrar carrusel de portadas locales en Home, eliminar scursor parpadeante

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ function Navbar() {
     { label: "Inicio", path: "/" },
     { label: "Servicios", path: "/servicios" },
     { label: "Acerca de", path: "/acerca" },
-    { label: "Publicaciones", path: "/publicaciones" },
+  
   ];
 
   const isActive = (path: string) => location.pathname === path;
